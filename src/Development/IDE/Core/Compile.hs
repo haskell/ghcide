efb92c1209bfbc99d8f9cf6334db3b56c583b5a2
Jacek Generowicz <jacg@my-post-office.net>
2019-09-14 10:25:59 +0200

    Defer out of scope variables
    ... passing the test which was changed in the last commit.

--- src/Development/IDE/Core/Compile.hs
+++ src/Development/IDE/Core/Compile.hs
@@ -135,6 +135,7 @@ demoteTypeErrorsToWarnings =
   demoteTEsToWarns :: DynFlags -> DynFlags
   demoteTEsToWarns = (`gopt_set` Opt_DeferTypeErrors)
                    . (`gopt_set` Opt_DeferTypedHoles)
+                   . (`gopt_set` Opt_DeferOutOfScopeVariables)
 
   update_hspp_opts :: (DynFlags -> DynFlags) -> ModSummary -> ModSummary
   update_hspp_opts up ms = ms{ms_hspp_opts = up $ ms_hspp_opts ms}
