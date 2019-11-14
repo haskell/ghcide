484d441066141085db35f0d4fd0b80a66c2259ec
Jinwoo Lee <jinwoo@google.com>
2019-11-14 11:23:14 -0800

    A separate function for initializing plugins

--- src/Development/IDE/Core/Compile.hs
+++ src/Development/IDE/Core/Compile.hs
@@ -99,19 +99,22 @@ typecheckModule (IdeDefer defer) packageState deps pm =
     runGhcEnv packageState $
         catchSrcErrors "typecheck" $ do
             setupEnv deps
-#if MIN_GHC_API_VERSION(8,6,0)
-            session <- getSession
             let modSummary = pm_mod_summary pm
-            dflags <- liftIO $ initializePlugins session (ms_hspp_opts modSummary)
-            let modSummary' = modSummary{ms_hspp_opts = dflags}
+            modSummary' <- initPlugins modSummary
             (warnings, tcm) <- withWarnings "typecheck" $ \tweak ->
                 GHC.typecheckModule $ demoteIfDefer pm{pm_mod_summary = tweak modSummary'}
-#else
-            (warnings, tcm) <- withWarnings "typecheck" $ \tweak ->
-                GHC.typecheckModule $ demoteIfDefer pm{pm_mod_summary = tweak $ pm_mod_summary pm}
-#endif
             tcm2 <- mkTcModuleResult tcm
             return (map unDefer warnings, tcm2)
+
+initPlugins :: GhcMonad m => ModSummary -> m ModSummary
+initPlugins modSummary = do
+#if MIN_GHC_API_VERSION(8,6,0)
+    session <- getSession
+    dflags <- liftIO $ initializePlugins session (ms_hspp_opts modSummary)
+    return modSummary{ms_hspp_opts = dflags}
+#else
+    return modSummary
+#endif
 
 -- | Compile a single type-checked module to a 'CoreModule' value, or
 -- provide errors.
