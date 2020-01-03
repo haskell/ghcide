haskell/haskell-language-server

commit 7e80629188ab9738b046a2fc83e428d3388a68e4

Fix performance regression introduced by filepath normalisation (#303)
We already normalise filepaths in NormalizedFilePath. haskell-lsp
changed things such that the conversion from Uri to NormalizedUri
normalises the filepath again which caused a significant slowdown in
GetFileExists.

We already have a wrapper for converting from NormalizedFilePath to
NormalizedUri so this PR changes this wrapper to inline the definition
without the additional layer of normalisation.

fixes #298

src/Development/IDE/Types/Location.hs
@@ -29,7 +29,12 @@ import Data.Binary
 import Data.Maybe as Maybe
 import Data.Hashable
 import Data.String
+import qualified Data.Text as T
+import Network.URI
 import System.FilePath
+import qualified System.FilePath.Posix as FPP
+import qualified System.FilePath.Windows as FPW
+import System.Info.Extra
 import qualified Language.Haskell.LSP.Types as LSP
 import Language.Haskell.LSP.Types as LSP (
     filePathToUri
@@ -65,7 +70,40 @@ uriToFilePath' uri
     | otherwise = LSP.uriToFilePath uri
 
 filePathToUri' :: NormalizedFilePath -> NormalizedUri
-filePathToUri' = toNormalizedUri . filePathToUri . fromNormalizedFilePath
+filePathToUri' (NormalizedFilePath fp) = toNormalizedUri $ Uri $ T.pack $ LSP.fileScheme <> "//" <> platformAdjustToUriPath fp
+  where
+    -- The definitions below are variants of the corresponding functions in Language.Haskell.LSP.Types.Uri that assume that
+    -- the filepath has already been normalised. This is necessary since normalising the filepath has a nontrivial cost.
+
+    toNormalizedUri :: Uri -> NormalizedUri
+    toNormalizedUri (Uri t) =
+      NormalizedUri $ T.pack $ escapeURIString isUnescapedInURI $ unEscapeString $ T.unpack t
+
+    platformAdjustToUriPath :: FilePath -> String
+    platformAdjustToUriPath srcPath
+      | isWindows = '/' : escapedPath
+      | otherwise = escapedPath
+      where
+        (splitDirectories, splitDrive)
+          | isWindows =
+              (FPW.splitDirectories, FPW.splitDrive)
+          | otherwise =
+              (FPP.splitDirectories, FPP.splitDrive)
+        escapedPath =
+            case splitDrive srcPath of
+                (drv, rest) ->
+                    convertDrive drv `FPP.joinDrive`
+                    FPP.joinPath (map (escapeURIString unescaped) $ splitDirectories rest)
+        -- splitDirectories does not remove the path separator after the drive so
+        -- we do a final replacement of \ to /
+        convertDrive drv
+          | isWindows && FPW.hasTrailingPathSeparator drv =
+              FPP.addTrailingPathSeparator (init drv)
+          | otherwise = drv
+        unescaped c
+          | isWindows = isUnreserved c || c `elem` [':', '\\', '/']
+          | otherwise = isUnreserved c || c == '/'
+
 
 
 fromUri :: LSP.NormalizedUri -> NormalizedFilePath

haskell/ghcide

commit c0e145e9178ece3b09b8863074cbe139cf97c122

Fix check for empty file path
I accidentally broke this on Windows in #303 by letting the two
conversirons get out of sync.

src/Development/IDE/Types/Location.hs
@@ -66,8 +66,11 @@ fromNormalizedFilePath (NormalizedFilePath fp) = fp
 -- So we have our own wrapper here that supports empty filepaths.
 uriToFilePath' :: Uri -> Maybe FilePath
 uriToFilePath' uri
-    | uri == filePathToUri "" = Just ""
+    | uri == fromNormalizedUri emptyPathUri = Just ""
     | otherwise = LSP.uriToFilePath uri
+
+emptyPathUri :: NormalizedUri
+emptyPathUri = filePathToUri' ""
 
 filePathToUri' :: NormalizedFilePath -> NormalizedUri
 filePathToUri' (NormalizedFilePath fp) = toNormalizedUri $ Uri $ T.pack $ LSP.fileScheme <> "//" <> platformAdjustToUriPath fp

test/exe/Main.hs
@@ -18,6 +18,7 @@ import Development.IDE.GHC.Util
 import qualified Data.Text as T
 import Development.IDE.Test
 import Development.IDE.Test.Runfiles
+import Development.IDE.Types.Location
 import Language.Haskell.LSP.Test
 import Language.Haskell.LSP.Types
 import Language.Haskell.LSP.Types.Capabilities
@@ -48,6 +49,7 @@ main = defaultMain $ testGroup "HIE"
   , pluginTests
   , preprocessorTests
   , thTests
+  , unitTests
   ]
 
 initializeResponseTests :: TestTree
@@ -1306,3 +1308,10 @@ openTestDataDoc :: FilePath -> Session TextDocumentIdentifier
 openTestDataDoc path = do
   source <- liftIO $ readFileUtf8 $ "test/data" </> path
   openDoc' path "haskell" source
+
+unitTests :: TestTree
+unitTests = do
+  testGroup "Unit"
+     [ testCase "empty file path" $
+         uriToFilePath' (fromNormalizedUri $ filePathToUri' "") @?= Just ""
+     ]