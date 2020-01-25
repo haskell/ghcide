df0d7ef33e5549e57a649391744f0a6364e0959f
Pepe Iborra <pepeiborra@gmail.com>
2020-01-25 09:07:50 +0000

    Switch to async implementation
    Off-sourcing the details of aborting computations to a mature library

--- src/Development/IDE/Core/Abortable.hs
+++ src/Development/IDE/Core/Abortable.hs
@@ -1,14 +1,10 @@
 module Development.IDE.Core.Abortable
   ( Abortable(..)
-  , spawnSimple
   , spawnWithContinuation
-  , spawnWithContinuation2
   ) where
 
 import qualified Control.Concurrent.Async as Async
-import           Control.Concurrent.Extra
 import           Control.Exception
-import           Control.Monad
 
 -- | A computation that can be aborted.
 data Abortable a = Abortable
@@ -17,29 +13,9 @@ data Abortable a = Abortable
   }
 
 -- | Spawns an abortable computation
-spawnSimple :: IO a -> IO (Abortable (Either SomeException a))
-spawnSimple act = do
-  bar      <- newBarrier
-  threadId <- forkFinally act $ signalBarrier bar
-  let abort = killThread threadId >> void (waitBarrier bar)
-      wait  = waitBarrier bar
-  return Abortable { .. }
-
--- | Spawns an abortable computation
 --   and runs the continuation when done.
---   The continuation runs masked.
 spawnWithContinuation :: (Either SomeException a -> IO b) -> IO a -> IO (Abortable b)
 spawnWithContinuation h act = do
-  bar      <- newBarrier
-  threadId <- forkFinally act (h >=> signalBarrier bar)
-  let abort = killThread threadId >> void (waitBarrier bar)
-      wait  = waitBarrier bar
-  return Abortable { .. }
-
--- | Spawns an abortable computation
---   and runs the continuation when done.
-spawnWithContinuation2 :: (Either SomeException a -> IO b) -> IO a -> IO (Abortable b)
-spawnWithContinuation2 h act = do
   worker <- Async.asyncWithUnmask $ \restore -> try(restore act) >>= h
   let abort = Async.cancel worker
       wait  = Async.wait worker
