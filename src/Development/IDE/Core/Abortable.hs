98f085cdd514c310478bfe6c7de15631d5916d5f
Pepe Iborra <pepeiborra@gmail.com>
2020-01-25 08:44:41 +0000

    Add an Abortable implementation with Async

--- src/Development/IDE/Core/Abortable.hs
+++ src/Development/IDE/Core/Abortable.hs
@@ -1,9 +1,11 @@
 module Development.IDE.Core.Abortable
   ( Abortable(..)
-  , startSimple
-  , startWithContinuation
+  , spawnSimple
+  , spawnWithContinuation
+  , spawnWithContinuation2
   ) where
 
+import qualified Control.Concurrent.Async as Async
 import           Control.Concurrent.Extra
 import           Control.Exception
 import           Control.Monad
@@ -15,8 +17,8 @@ data Abortable a = Abortable
   }
 
 -- | Spawns an abortable computation
-startSimple :: IO a -> IO (Abortable (Either SomeException a))
-startSimple act = do
+spawnSimple :: IO a -> IO (Abortable (Either SomeException a))
+spawnSimple act = do
   bar      <- newBarrier
   threadId <- forkFinally act $ signalBarrier bar
   let abort = killThread threadId >> void (waitBarrier bar)
@@ -26,10 +28,19 @@ startSimple act = do
 -- | Spawns an abortable computation
 --   and runs the continuation when done.
 --   The continuation runs masked.
-startWithContinuation :: (Either SomeException a -> IO b) -> IO a -> IO (Abortable b)
-startWithContinuation h act = do
+spawnWithContinuation :: (Either SomeException a -> IO b) -> IO a -> IO (Abortable b)
+spawnWithContinuation h act = do
   bar      <- newBarrier
   threadId <- forkFinally act (h >=> signalBarrier bar)
   let abort = killThread threadId >> void (waitBarrier bar)
       wait  = waitBarrier bar
   return Abortable { .. }
+
+-- | Spawns an abortable computation
+--   and runs the continuation when done.
+spawnWithContinuation2 :: (Either SomeException a -> IO b) -> IO a -> IO (Abortable b)
+spawnWithContinuation2 h act = do
+  worker <- Async.asyncWithUnmask $ \restore -> try(restore act) >>= h
+  let abort = Async.cancel worker
+      wait  = Async.wait worker
+  return Abortable {..}
