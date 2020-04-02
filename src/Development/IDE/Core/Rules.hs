66f494f9d7f9217455b68f0b8ea83fda1c49f583
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-04-02 14:35:25 +0100

    [#518] Build ghcide with GHC 8.10.1
    Resolves #518

--- src/Development/IDE/Core/Rules.hs
+++ src/Development/IDE/Core/Rules.hs
@@ -536,7 +536,11 @@ getHiFileRule = defineEarlyCutoff $ \GetHiFile f -> do
               case r of
                 Right iface -> do
                   let result = HiFileResult ms iface
+#if MIN_GHC_API_VERSION(8,10,0)
+                  return (Just (fingerprintToBS (mi_mod_hash $ mi_final_exts iface)), ([], Just result))
+#else
                   return (Just (fingerprintToBS (mi_mod_hash iface)), ([], Just result))
+#endif
                 Left err -> do
                   let diag = ideErrorWithSource (Just "interface file loading") (Just DsError) f . T.pack $ err
                   return (Nothing, (pure diag, Nothing))
