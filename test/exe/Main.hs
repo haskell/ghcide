4eaedd5a9298e273e5bee576d2c2c724355cbe33
Matthew Pickering <matthewtpickering@gmail.com>
2020-06-08 21:58:48 +0100

    Love a good hlint failure to waste my time

--- test/exe/Main.hs
+++ test/exe/Main.hs
@@ -1378,7 +1378,7 @@ findDefinitionAndHoverTests = let
     [ testGroup "definition" $ mapMaybe fst tests
     , testGroup "hover"      $ mapMaybe snd tests
     , checkFileCompiles sourceFilePath
-    , testGroup "type-definition" $ typeDefinitionTests ]
+    , testGroup "type-definition" typeDefinitionTests ]
 
   typeDefinitionTests = [ tst (getTypeDefinitions, checkDefs) aaaL14 (pure tcData) "Saturated data con"
                         , tst (getTypeDefinitions, checkDefs) opL16 (pure [ExpectNoDefinitions]) "Polymorphic variable"]
