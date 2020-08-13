03ec6fed12db8b58b41c8ab028936cda10aeb814
Pepe Iborra <pepeiborra@gmail.com>
2020-08-13 20:40:21 +0100

    Formatting

--- src/Development/IDE/Types/Action.hs
+++ src/Development/IDE/Types/Action.hs
@@ -1,69 +1,75 @@
 module Development.IDE.Types.Action
-  (DelayedAction(..)
-  ,DelayedActionInternal
-  ,ActionQueue
-  ,newQueue
-  ,pushQueue
-  ,popQueue
-  ,doneQueue
-  ,peekInProgress
-  )where
+  ( DelayedAction (..),
+    DelayedActionInternal,
+    ActionQueue,
+    newQueue,
+    pushQueue,
+    popQueue,
+    doneQueue,
+    peekInProgress,
+  )
+where
 
-import Development.IDE.Types.Logger
-import Development.Shake (Action)
-import Control.Concurrent.STM (readTVar, modifyTVar, readTQueue, atomically, newTQueue, newTVar, writeTQueue, TQueue, STM, TVar)
-import Data.Unique (Unique)
-import Data.Hashable (Hashable(..))
-import Data.HashSet (HashSet)
-import qualified Data.HashSet as Set
+import           Control.Concurrent.STM       (STM, TQueue, TVar, atomically,
+                                               modifyTVar, newTQueue, newTVar,
+                                               readTQueue, readTVar,
+                                               writeTQueue)
+import           Data.Hashable                (Hashable (..))
+import           Data.HashSet                 (HashSet)
+import qualified Data.HashSet                 as Set
+import           Data.Unique                  (Unique)
+import           Development.IDE.Types.Logger
+import           Development.Shake            (Action)
 
 data DelayedAction a = DelayedAction
-  { uniqueID  :: Maybe Unique
-  , actionName :: String -- ^ Name we use for debugging
-  , actionPriority :: Priority -- ^ Priority with which to log the action
-  , getAction :: Action a -- ^ The payload
+  { uniqueID       :: Maybe Unique,
+    -- | Name we use for debugging
+    actionName     :: String,
+    -- | Priority with which to log the action
+    actionPriority :: Priority,
+    -- | The payload
+    getAction      :: Action a
   }
-  deriving Functor
+  deriving (Functor)
 
 type DelayedActionInternal = DelayedAction ()
 
 instance Eq (DelayedAction a) where
-
-    a == b = uniqueID a == uniqueID b
+  a == b = uniqueID a == uniqueID b
 
 instance Hashable (DelayedAction a) where
-    hashWithSalt s = hashWithSalt s . uniqueID
+  hashWithSalt s = hashWithSalt s . uniqueID
 
 instance Show (DelayedAction a) where
-    show d = "DelayedAction: " ++ actionName d
+  show d = "DelayedAction: " ++ actionName d
 
 ------------------------------------------------------------------------------
 
-data ActionQueue = ActionQueue {
-    newActions :: TQueue DelayedActionInternal,
+data ActionQueue = ActionQueue
+  { newActions :: TQueue DelayedActionInternal,
     inProgress :: TVar (HashSet DelayedActionInternal)
   }
 
 newQueue :: IO ActionQueue
 newQueue = atomically $ do
-    newActions <- newTQueue
-    inProgress <- newTVar mempty
-    return ActionQueue{..}
+  newActions <- newTQueue
+  inProgress <- newTVar mempty
+  return ActionQueue {..}
 
 pushQueue :: DelayedActionInternal -> ActionQueue -> STM ()
-pushQueue act ActionQueue{..} = writeTQueue newActions act
+pushQueue act ActionQueue {..} = writeTQueue newActions act
 
 -- | You must call 'doneQueue' to signal completion
 popQueue :: ActionQueue -> STM DelayedActionInternal
-popQueue ActionQueue{..} = do
-    x <- readTQueue newActions
-    modifyTVar inProgress (Set.insert x)
-    return x
+popQueue ActionQueue {..} = do
+  x <- readTQueue newActions
+  modifyTVar inProgress (Set.insert x)
+  return x
 
 -- | Completely remove an action from the queue
 doneQueue :: DelayedActionInternal -> ActionQueue -> STM ()
-doneQueue x ActionQueue{..} =
-    modifyTVar inProgress (Set.delete x)
+doneQueue x ActionQueue {..} =
+  modifyTVar inProgress (Set.delete x)
 
 peekInProgress :: ActionQueue -> STM [DelayedActionInternal]
-peekInProgress ActionQueue{..} = Set.toList <$> readTVar inProgress
+peekInProgress ActionQueue {..} = Set.toList <$> readTVar inProgress
