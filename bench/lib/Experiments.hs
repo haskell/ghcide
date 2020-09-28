0d7a18bec1d3df83e7fbf0a7b1fde43e75a9f2c3
Pepe Iborra <pepeiborra@gmail.com>
2020-09-28 21:01:58 +0100

    No need for hardcoded experiment positions

--- bench/lib/Experiments.hs
+++ bench/lib/Experiments.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ImpredicativeTypes #-}
 
 module Experiments
 ( Bench(..)
@@ -23,10 +25,13 @@ import Control.Concurrent
 import Control.Exception.Safe
 import Control.Monad.Extra
 import Control.Monad.IO.Class
+import Data.Aeson (Value(Null))
 import Data.Char (isDigit)
 import Data.List
 import Data.Maybe
+import qualified Data.Text as T
 import Data.Version
+import Development.IDE.Plugin.Test
 import Experiments.Types
 import Language.Haskell.LSP.Test
 import Language.Haskell.LSP.Types
@@ -34,45 +39,38 @@ import Language.Haskell.LSP.Types.Capabilities
 import Numeric.Natural
 import Options.Applicative
 import System.Directory
+import System.Environment.Blank (getEnv)
 import System.FilePath ((</>))
 import System.Process
 import System.Time.Extra
 import Text.ParserCombinators.ReadP (readP_to_S)
-import System.Environment.Blank (getEnv)
-import Development.IDE.Plugin.Test
-import Data.Aeson (Value(Null))
 
--- Points to a string in the target file,
--- convenient for hygienic edits
-hygienicP :: Position
-hygienicP = Position 854 23
-
-hygienicEdit :: TextDocumentContentChangeEvent
+hygienicEdit :: (?hygienicP :: Position) => TextDocumentContentChangeEvent
 hygienicEdit =
     TextDocumentContentChangeEvent
-    { _range = Just (Range hygienicP hygienicP),
+    { _range = Just (Range ?hygienicP ?hygienicP),
         _rangeLength = Nothing,
         _text = " "
     }
 
-breakingEdit :: TextDocumentContentChangeEvent
+breakingEdit :: (?identifierP :: Position) => TextDocumentContentChangeEvent
 breakingEdit =
     TextDocumentContentChangeEvent
-    { _range = Just (Range identifierP identifierP),
+    { _range = Just (Range ?identifierP ?identifierP),
         _rangeLength = Nothing,
         _text = "a"
     }
 
--- Points to the middle of an identifier,
--- convenient for requesting goto-def, hover and completions
-identifierP :: Position
-identifierP = Position 853 12
+-- | Experiments have access to these special positions:
+-- - hygienicP points to a string in the target file, convenient for hygienic edits
+-- - identifierP points to the middle of an identifier, convenient for goto-def, hover and completions
+type HasPositions = (?hygienicP :: Position, ?identifierP :: Position)
 
 experiments :: [Bench]
 experiments =
     [ ---------------------------------------------------------------------------------------
       bench "hover" 10 $ \doc ->
-        isJust <$> getHover doc identifierP,
+        isJust <$> getHover doc ?identifierP,
       ---------------------------------------------------------------------------------------
       bench "edit" 10 $ \doc -> do
         changeDoc doc [hygienicEdit]
@@ -81,10 +79,10 @@ experiments =
       ---------------------------------------------------------------------------------------
       bench "hover after edit" 10 $ \doc -> do
         changeDoc doc [hygienicEdit]
-        isJust <$> getHover doc identifierP,
+        isJust <$> getHover doc ?identifierP,
       ---------------------------------------------------------------------------------------
       bench "getDefinition" 10 $ \doc ->
-        not . null <$> getDefinitions doc identifierP,
+        not . null <$> getDefinitions doc ?identifierP,
       ---------------------------------------------------------------------------------------
       bench "documentSymbols" 100 $
         fmap (either (not . null) (not . null)) . getDocumentSymbols,
@@ -95,7 +93,7 @@ experiments =
       ---------------------------------------------------------------------------------------
       bench "completions after edit" 10 $ \doc -> do
         changeDoc doc [hygienicEdit]
-        not . null <$> getCompletions doc identifierP,
+        not . null <$> getCompletions doc ?identifierP,
       ---------------------------------------------------------------------------------------
       benchWithSetup
         "code actions"
@@ -103,7 +101,7 @@ experiments =
         ( \doc -> do
             changeDoc doc [breakingEdit]
             waitForProgressDone
-            return identifierP
+            return ?identifierP
         )
         ( \p doc -> do
             not . null <$> getCodeActions doc (Range p p)
@@ -114,7 +112,7 @@ experiments =
         10
         ( \doc -> do
             changeDoc doc [breakingEdit]
-            return identifierP
+            return ?identifierP
         )
         ( \p doc -> do
             changeDoc doc [hygienicEdit]
@@ -183,8 +181,8 @@ data Bench = forall setup.
   { name :: !String,
     enabled :: !Bool,
     samples :: !Natural,
-    benchSetup :: TextDocumentIdentifier -> Session setup,
-    experiment :: setup -> Experiment
+    benchSetup :: HasPositions => TextDocumentIdentifier -> Session setup,
+    experiment :: HasPositions => setup -> Experiment
   }
 
 select :: HasConfig => Bench -> Bool
@@ -196,14 +194,14 @@ select Bench {name, enabled} =
 benchWithSetup ::
   String ->
   Natural ->
-  (TextDocumentIdentifier -> Session p) ->
-  (p -> Experiment) ->
+  (HasPositions => TextDocumentIdentifier -> Session p) ->
+  (HasPositions => p -> Experiment) ->
   Bench
 benchWithSetup name samples benchSetup experiment = Bench {..}
   where
     enabled = True
 
-bench :: String -> Natural -> Experiment -> Bench
+bench :: String -> Natural -> (HasPositions => Experiment) -> Bench
 bench name defSamples userExperiment =
   benchWithSetup name defSamples (const $ pure ()) experiment
   where
@@ -319,53 +317,74 @@ waitForProgressDone =
 runBench ::
   (?config :: Config) =>
   (Session BenchRun -> IO BenchRun) ->
-  Bench ->
+  (HasPositions => Bench) ->
   IO BenchRun
-runBench runSess Bench {..} = handleAny (\e -> print e >> return badRun)
+runBench runSess b = handleAny (\e -> print e >> return badRun)
   $ runSess
   $ do
     doc <- openDoc exampleModulePath "haskell"
-    (startup, _) <- duration $ do
-      waitForProgressDone
-      -- wait again, as the progress is restarted once while loading the cradle
-      -- make an edit, to ensure this doesn't block
-      changeDoc doc [hygienicEdit]
-      waitForProgressDone
 
-    liftIO $ output $ "Running " <> name <> " benchmark"
-    (runSetup, userState) <- duration $ benchSetup doc
-    let loop !userWaits !delayedWork 0 = return $ Just (userWaits, delayedWork)
-        loop !userWaits !delayedWork n = do
-          (t, res) <- duration $ experiment userState doc
-          if not res
-            then return Nothing
-            else do
-              output (showDuration t)
-              -- Wait for the delayed actions to finish
-              waitId <- sendRequest (CustomClientMethod "test") WaitForShakeQueue
-              (td, resp) <- duration $ skipManyTill anyMessage $ responseForId waitId
-              case resp of
-                  ResponseMessage{_result=Right Null} -> do
-                    loop (userWaits+t) (delayedWork+td) (n -1)
-                  _ ->
-                  -- Assume a ghcide build lacking the WaitForShakeQueue command
-                    loop (userWaits+t) delayedWork (n -1)
+    -- Setup the special positions used by the experiments
+    lastLine <- length . T.lines <$> documentContents doc
+    changeDoc doc [TextDocumentContentChangeEvent
+        { _range = Just (Range (Position lastLine 0) (Position lastLine 0))
+        , _rangeLength = Nothing
+        , _text = T.unlines
+               [ "_hygienic = \"hygienic\""
+               , "_identifier = _hygienic"
+               ]
+        }]
+    let
+      -- Points to a string in the target file,
+      -- convenient for hygienic edits
+      ?hygienicP = Position lastLine 15
+    let
+      -- Points to the middle of an identifier,
+      -- convenient for requesting goto-def, hover and completions
+      ?identifierP = Position (lastLine+1) 15
 
-    (runExperiment, result) <- duration $ loop 0 0 samples
-    let success = isJust result
-        (userWaits, delayedWork) = fromMaybe (0,0) result
+    case b of
+     Bench{..} -> do
+      (startup, _) <- duration $ do
+        waitForProgressDone
+        -- wait again, as the progress is restarted once while loading the cradle
+        -- make an edit, to ensure this doesn't block
+        changeDoc doc [hygienicEdit]
+        waitForProgressDone
 
-    -- sleep to give ghcide a chance to GC
-    liftIO $ threadDelay 1100000
+      liftIO $ output $ "Running " <> name <> " benchmark"
+      (runSetup, userState) <- duration $ benchSetup doc
+      let loop !userWaits !delayedWork 0 = return $ Just (userWaits, delayedWork)
+          loop !userWaits !delayedWork n = do
+            (t, res) <- duration $ experiment userState doc
+            if not res
+              then return Nothing
+              else do
+                output (showDuration t)
+                -- Wait for the delayed actions to finish
+                waitId <- sendRequest (CustomClientMethod "test") WaitForShakeQueue
+                (td, resp) <- duration $ skipManyTill anyMessage $ responseForId waitId
+                case resp of
+                    ResponseMessage{_result=Right Null} -> do
+                      loop (userWaits+t) (delayedWork+td) (n -1)
+                    _ ->
+                    -- Assume a ghcide build lacking the WaitForShakeQueue command
+                      loop (userWaits+t) delayedWork (n -1)
 
-    (maxResidency, allocations) <- liftIO $
-        ifM (doesFileExist gcStats)
-            (parseMaxResidencyAndAllocations <$> readFile gcStats)
-            (pure (0,0))
+      (runExperiment, result) <- duration $ loop 0 0 samples
+      let success = isJust result
+          (userWaits, delayedWork) = fromMaybe (0,0) result
+          gcStats = escapeSpaces (name <> ".benchmark-gcStats")
 
-    return BenchRun {..}
-  where
-    gcStats = escapeSpaces (name <> ".benchmark-gcStats")
+      -- sleep to give ghcide a chance to GC
+      liftIO $ threadDelay 1100000
+
+      (maxResidency, allocations) <- liftIO $
+          ifM (doesFileExist gcStats)
+              (parseMaxResidencyAndAllocations <$> readFile gcStats)
+              (pure (0,0))
+
+      return BenchRun {..}
 
 data SetupResult = SetupResult {
     runBenchmarks :: [Bench] -> IO (),
@@ -423,6 +442,7 @@ setup = do
   let cleanUp = case example ?config of
         GetPackage{} -> removeDirectoryRecursive examplesPath
         UsePackage{} -> return ()
+
       runBenchmarks = runBenchmarksFun benchDir
 
   return SetupResult{..}
