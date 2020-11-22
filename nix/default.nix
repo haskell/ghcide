3e3ca5b6a02e300d9e1138850586a861e9e95a3e
Pepe Iborra <pepeiborra@gmail.com>
2020-11-22 10:25:31 +0000

    [nix] disable tests and jailbreak 8.10.x packages

--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,28 @@
 { sources ? import ./sources.nix }:
 let
-  overlay = self: pkgs:
-  {
-    haskell = pkgs.haskell // {
-      packageOverrides = selfPkgs: superPkgs: {
+  overlay = _self: pkgs:
+    let sharedOverrides = {
+        overrides = _self: super: {
+            mkDerivation = args: super.mkDerivation (args //
+                {
+                    # skip running tests for Hackage packages
+                    doCheck =
+                        # but not for ghcide
+                        args.version == "0";
+                    # relax upper bounds
+                    jailbreak = args.pname != "jailbreak-cabal";
+                });
             };
-          };
+            };
+        in
+        {
+        haskell = pkgs.haskell // {
+            packages = pkgs.haskell.packages // {
+                # relax upper bounds on ghc 8.10.x versions (and skip running tests)
+                ghc8101 = pkgs.haskell.packages.ghc8101.override sharedOverrides;
+                ghc8102 = pkgs.haskell.packages.ghc8102.override sharedOverrides;
+            };
+        };
         };
 
 in import sources.nixpkgs
