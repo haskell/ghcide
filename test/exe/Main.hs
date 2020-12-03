57c09e672791c9435a11cf4e128cfba7dfbd9eaf
grdvnl <grdvnl@gmail.com>
2020-12-03 05:47:35 -0800

    Add test adding imports to empty list

--- test/exe/Main.hs
+++ test/exe/Main.hs
@@ -2886,6 +2886,13 @@ nonLocalCompletionTests =
       [("join", CiFunction, "join ${1:m (m a)}", False, False,
         Just (List [TextEdit {_range = Range {_start = Position {_line = 3, _character = 8}, _end = Position {_line = 3, _character = 8}}, _newText = ", join"}]))],
     completionTest
+      "show imports not in list - initial empty list"
+      ["{-# LANGUAGE NoImplicitPrelude #-}",
+       "module A where", "import qualified Control.Monad as M ()", "f = M.joi"]
+      (Position 3 10)
+      [("join", CiFunction, "join ${1:m (m a)}", False, False,
+        Just (List [TextEdit {_range = Range {_start = Position {_line = 2, _character = 37}, _end = Position {_line = 2, _character = 37}}, _newText = "join"}]))],
+    completionTest
        "dont show hidden items"
        [ "{-# LANGUAGE NoImplicitPrelude #-}",
          "module A where",
